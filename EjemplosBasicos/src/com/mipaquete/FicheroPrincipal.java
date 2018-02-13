0b370dfb98ff219f5b0d5b22a86788657721d704
Germán Caballero Rodríguez <germanux@gmail.com>
2018-02-13 10:23:17 +0100

    Ejemplos basicos
    funciones y variables

---
+++ EjemplosBasicos/src/com/mipaquete/FicheroPrincipal.java
@@ -0,0 +1,14 @@
+package com.mipaquete;
+import com.mipaquete.EjemplosBooleanos;
+import com.mipaquete.EjemplosFunciones;
+
+public class FicheroPrincipal {
+ 
+	public static void main(String[] argumentos) {
+		// TODO Auto-generated method stub
+		System.out.println("Programa de ejemplos");
+
+		// EjemplosFunciones.main(args);
+		EjemplosBooleanos.funcionPrincipal();
+	}
+}
