2d393bed33020d5dc89abd80d3f7d3fdebb4d2b7
German <germanux@gmail.com>
2018-02-16 15:01:48 +0100

    Haciendo que muestre bien la palabra oculta, con GUIONES.

--- ProgramaAhorcado/src/com/incyde/JuegoPrincipal.java
+++ ProgramaAhorcado/src/com/incyde/JuegoPrincipal.java
@@ -1,18 +1,31 @@
 package com.incyde;
+//TODO: Preguntar si letra o palabra
 
 public class JuegoPrincipal {
 	static String palabraElegida;
 	static String letra;
 	static String guiones;
+	static String nuevosGuiones;
+	static String nuevosGuionesAnterior;
 	static int longitudPalabra;
 
-	//TODO: Preguntar si letra o palabra
-	
+	static String soloGuiones_A() {
+		String guiones = "";
+		for (int i = 0; i < longitudPalabra; i++) {
+			guiones = guiones + "-";
+		}
+		return guiones;
+	}
+	static String soloGuiones_B() {
+		String guiones = "-----------".substring(0, longitudPalabra);
+		return guiones;
+	}
 	public static void main(String[] args) {
 		palabraElegida = ListaPalabras.palabraAleatoria();
-		longitudPalabra = palabraElegida.length();		
-		System.out.println("La palabra es " + ListaPalabras.palabraAleatoria());
-		guiones = ""; 
+		longitudPalabra = palabraElegida.length();	
+		//System.out.println("La palabra es " + ListaPalabras.palabraAleatoria());
+		guiones = soloGuiones_A(); 		
+		nuevosGuionesAnterior =  guiones;
 		 
 		while(true ) {
 			letra = VistaYTeclado.pedirLetra();
@@ -21,24 +34,31 @@ public class JuegoPrincipal {
 			if (palabraElegida.contains(letra)) {
 				cuandoAciertaLetra();
 			} else {
-				System.out.println("No contiene");
+				System.out.println("No contiene. Has fallado");
 			}
 		}		
 	}
+
+	
 	static void cuandoAciertaLetra() {
 		int posicion;
 		String letraPos;
+		nuevosGuiones = "";
 		for (posicion = 0; posicion < longitudPalabra; posicion++) {
 			letraPos = String.valueOf(palabraElegida.charAt(posicion));
 			
-			if (letra.equals(letraPos)) {
-				System.out.println("pos: " + posicion + ", let: " +letraPos);
-				guiones = guiones + letraPos;
-			} else {
-				guiones = guiones + "-";
+			if (letra.equals(letraPos)) { // A
+				// System.out.println("pos: " + posicion + ", let: " +letraPos);
+				nuevosGuiones = nuevosGuiones + letraPos;
+			} else { // X
+				
+				nuevosGuiones = nuevosGuiones + nuevosGuionesAnterior.charAt(posicion);
+				
 			}
-			System.out.println(guiones);
+			
 		}
+		nuevosGuionesAnterior = nuevosGuiones;
+		System.out.println(nuevosGuiones);
 	}
 	void pruebasString() {
 		/*System.out.println(palabraElegida.charAt(2));
--- ProgramaAhorcado/src/com/incyde/ListaPalabras.java
+++ ProgramaAhorcado/src/com/incyde/ListaPalabras.java
@@ -2,10 +2,10 @@ package com.incyde;
 
 public class ListaPalabras {
 	
-	static String[] palabras = {"vaso", "luz", "pan", "ordenador"};
+	static String[] palabras = {"pilum", "gladius", "guion", "array","hipopotomonstrosesquipedaliofobia"};
 	
 	public static String palabraAleatoria() {
 		// De momento, dame el primer elemento
-		return palabras[3]; 
+		return palabras[4]; 
 	}
 }
